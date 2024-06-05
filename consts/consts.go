92841eeea6a616dcc75cffa108357a5c40d6abcc
AmazingLeann <nwaforchukwudalu01@gmail.com>
2024-06-05 04:12:54 +0000

    chore: add updates

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Amazing Hyperchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Amaz"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "AMAZHYPER"
 )
 
 var ID ids.ID
